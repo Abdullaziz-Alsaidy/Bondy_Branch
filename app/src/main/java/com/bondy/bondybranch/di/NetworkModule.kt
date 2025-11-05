1f9ec5b543be57f49af8dcde1af1558ba56efdca
Abdullaziz-Alsaidy <abdullaziz.s.alsaidy@gmail.com>
2025-11-05 23:16:16 +0300

    Mock Data

--- app/src/main/java/com/bondy/bondybranch/di/NetworkModule.kt
+++ app/src/main/java/com/bondy/bondybranch/di/NetworkModule.kt
@@ -22,7 +22,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private const val BASE_URL = "https://fake.bondy.local/api/"
+    private const val BASE_URL = "http://10.0.2.2:8011/"
 
     @Provides
     @Singleton
